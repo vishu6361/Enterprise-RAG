47951e059db921257200829b38a81025ab3d6feb
Vishwajeet Kumar <vishwajeet.kumar@magicbricks.com>
2026-08-18 21:42:03 +0530

    Initial setup of spring boot

---
+++ backend/enterprise-rag/src/main/java/com/vish/enterprise_rag/EnterpriseRagApplication.java
@@ -0,0 +1,13 @@
+package com.vish.enterprise_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseRagApplication.class, args);
+	}
+
+}
